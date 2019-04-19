508bdf4d31bf2e0f65429fc93aff47ea25724ac4
fedamo <fedamo@gmail.com>
2019-04-19 14:27:19 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mysite/precache-manifest.505f376b56d39730b987eeafbc3535e8.js"
+  "/mysite/precache-manifest.39367b7567f41d42f201d8689df66af1.js"
 );
 
 workbox.clientsClaim();
